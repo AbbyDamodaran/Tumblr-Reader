fede1c9daea40e3c5bb9d3dd08d453831b878a64
Abby Damodaran <abbydamodaran@Abbys-MacBook-Air.local>
2023-10-20 21:26:33 -0700

    Initial commit with required features

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-## Project 6: Tumblr Detail Starter
+# Project 6 - *Tumblr Blog*
+
+Submitted by: **Abinaya Damodaran**
+
+**Tumblr Blog** is an app that displays the latest posts from your favorite Tumblr blog.
+
+Time spent: **3** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] Setup navigation to the Detail Screen
+- [x] Created the Detail View UI
+- [x] Add the ability to pass data to the Detail View Controller
+- [x] Made personal finishing touches to the UI: changed textview font size & color to dark grey, added large title to app feed (navigation bar title), added code to unselect row when returning from the detail view, changed imga eview content mode from 'aspect fit' to 'aspect fill', changed look of the main table view with a gradient opacity image
+
+## Video Walkthrough
+
+[Demo]](https://i.imgur.com/vULe45o.gif)
+
+## Notes
+
+An easy but satisfying project!
+
+Here are some code references I used for UI styling:
+Gradient alpha in image view: https://stackoverflow.com/questions/24026981/transparency-gradient-on-uiimageview
+Black text with hite outline in UILabel: https://stackoverflow.com/questions/1103148/how-do-i-make-uilabel-display-outlined-text
+Displaying text on top of an image: https://stackoverflow.com/questions/34972930/make-uilabel-appear-on-top-of-uiimageview-in-storyboard
+
+
+## License
+
+    Copyright [2023] [Abinaya Damodaran]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- ios101-project6-tumblr/ViewController.swift
+++ ios101-project6-tumblr/ViewController.swift
@@ -19,6 +19,32 @@ class ViewController: UIViewController, UITableViewDataSource {
         fetchPosts()
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+
+            // Deselect currently selected row
+            tableView.deselectRow(at: selectedIndexPath, animated: animated)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // MARK: - Pass the selected post data
+
+        // Get the index path for the selected row.
+        // `indexPathForSelectedRow` returns an optional `indexPath`, so we'll unwrap it with a guard.
+        guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
+
+        // Get the selected movie from the movies array using the selected index path's row
+        let selectedPost = posts[selectedIndexPath.row]
+
+        // Get access to the detail view controller via the segue's destination. (guard to unwrap the optional)
+        guard let detailViewController = segue.destination as? DetailViewController else { return }
+
+        detailViewController.post = selectedPost
+    }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
@@ -28,6 +54,21 @@ class ViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
 
         let post = posts[indexPath.row]
+        
+        let strokeTextAttributes: [NSAttributedString.Key : Any] = [
+            .strokeColor : UIColor.white,
+            .foregroundColor : UIColor.black,
+            .strokeWidth : -0.5,
+            ]
+
+        cell.summaryLabel.attributedText = NSAttributedString(string: "Foo", attributes: strokeTextAttributes)
+        
+        let maskLayer = CAGradientLayer(layer: cell.postImageView.layer)
+        maskLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        maskLayer.startPoint = CGPoint(x: 0.9, y: 0)
+        maskLayer.endPoint = CGPoint(x: 0, y: 0)
+        maskLayer.frame = cell.postImageView.bounds
+        cell.postImageView.layer.mask = maskLayer
 
         cell.summaryLabel.text = post.summary
 
